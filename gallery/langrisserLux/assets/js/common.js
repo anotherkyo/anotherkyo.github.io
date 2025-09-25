ca526d8341f23dc7c1ee305b57ba82c58c859f93
anotherkyo <120743251+anotherkyo@users.noreply.github.com>
2025-09-26 00:52:45 +0900

    Add files via upload

--- gallery/langrisserLux/assets/js/common.js
+++ gallery/langrisserLux/assets/js/common.js
@@ -16,27 +16,29 @@ const images = [
   { src: "https://i.imgur.com/Ti31qaR.png", full: "https://i.imgur.com/Ti31qaR.png", title: "전생의 글귀-아니에스(티스라오)", tags: ["투구","바람","전용장비"] },
   { src: "https://i.imgur.com/qRhO6i7.png", full: "https://i.imgur.com/qRhO6i7.png", title: "묠니르-아니에스(티스라오)", tags: ["무기","만월"] },
   { src: "https://i.imgur.com/x31FayX.png", full: "https://i.imgur.com/x31FayX.png", title: "피의 맹약-아니에스(티스라오)", tags: ["장신구","만월"] },
-  { src: "https://i.imgur.com/u0sxNJj.png", full: "https://i.imgur.com/u0sxNJj.png", title: "티아메트의 가호-아니에스(티스라오)", tags: ["방어구","아니에스"] },
+  //{ src: "https://i.imgur.com/u0sxNJj.png", full: "https://i.imgur.com/u0sxNJj.png", title: "티아메트의 가호-아니에스(티스라오)", tags: ["방어구","만월"] },
   { src: "https://i.imgur.com/5jECxCI.png", full: "https://i.imgur.com/5jECxCI.png", title: "흡혈귀 가면-마요네즈눈사람(노센레이어)", tags: ["투구","바람"] },
   { src: "https://i.imgur.com/080TlC9.png", full: "https://i.imgur.com/080TlC9.png", title: "모래바람의 인연-Vayu(어둠의 전설)", tags: ["장신구","강철"] },
   { src: "https://i.imgur.com/z5UrJ7e.png", full: "https://i.imgur.com/z5UrJ7e.png", title: "붉은 달-알약콩(레이갈드)", tags: ["무기","시계"] },
   { src: "https://i.imgur.com/p5WfjCl.png", full: "https://i.imgur.com/p5WfjCl.png", title: "티르의분노-Sukwind(티스라오)", tags: ["투구","바람"] },
-  { src: "https://i.imgur.com/Z45lBZL.png", full: "https://i.imgur.com/Z45lBZL.png", title: "기적의 지팡이-멀티팩터(발디아)", tags: ["무기","태양"] },
+  //{ src: "https://i.imgur.com/Z45lBZL.png", full: "https://i.imgur.com/Z45lBZL.png", title: "기적의 지팡이-멀티팩터(발디아)", tags: ["무기","태양"] },
   { src: "https://i.imgur.com/nhxCUsZ.png", full: "https://i.imgur.com/nhxCUsZ.png", title: "티타늄 부츠-갑꾸꾸(티스라오)", tags: ["장신구","바람"] },
   { src: "https://i.imgur.com/PeNtO9J.png", full: "https://i.imgur.com/PeNtO9J.png", title: "정의의 선서-Elio(티스라오)", tags: ["무기","강철"] },
   { src: "https://i.imgur.com/NJZhLlj.png", full: "https://i.imgur.com/NJZhLlj.png", title: "천녀의 날개옷-Gogi(티스라오)", tags: ["방어구","시계"] },
   { src: "https://i.imgur.com/CCHj7lF.png", full: "https://i.imgur.com/CCHj7lF.png", title: "빨간 리본-폐관수련중(티스라오)", tags: ["투구","바람"] },
   { src: "https://i.imgur.com/cBY6OEv.png", full: "https://i.imgur.com/cBY6OEv.png", title: "마물 헌터캡-폐관수련중(티스라오)", tags: ["투구","만월"] },
   { src: "https://i.imgur.com/WBrEmo3.png", full: "https://i.imgur.com/WBrEmo3.png", title: "성검 훈장-니나노오피스7o(노센레이어)", tags: ["장신구","바람"] },
-  { src: "https://i.imgur.com/jHOqbPE.png", full: "https://i.imgur.com/jHOqbPE.png", title: "미미르의 망치-망고리6(발디아)", tags: ["무기","바람"] }
-  
+  { src: "https://i.imgur.com/jHOqbPE.png", full: "https://i.imgur.com/jHOqbPE.png", title: "미미르의 망치-망고리6(발디아)", tags: ["무기","바람"] },
+  { src: "https://i.imgur.com/alk8NDa.png", full: "https://i.imgur.com/alk8NDa.png", title: "티아메트의 가호-작은배추(레이갈드)", tags: ["방어구","만월"] },
+  { src: "https://i.imgur.com/exgXEF2.png", full: "https://i.imgur.com/exgXEF2.png", title: "기적의 지팡이-진영편성(티스라오)", tags: ["무기","태양"] },
+  { src: "https://i.imgur.com/msXAQVd.png", full: "https://i.imgur.com/msXAQVd.png", title: "자객의 활-진영편성(티스라오)", tags: ["무기","바람"] } 
 ];
 
 
 const gallery = document.getElementById('gallery');
 const loadMoreBtn = document.getElementById('load-more');
 let loadedCount = 0;
-const pageSize = 25;
+const pageSize = 28;
 
 function renderMore() {
   const end = Math.min(loadedCount + pageSize, images.length);
@@ -148,6 +150,3 @@ window.addEventListener('load',()=>{
   const p=new URL(location).searchParams.get('img');
   if(p!==null){setTimeout(()=>openAt(Number(p)),500);}
 });
-
-
-
